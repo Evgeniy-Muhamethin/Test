0a936e3be93d5aff99b1119031b4d53b96d27d23
Evgeniy-Muhamethin <John090918@gmail.com>
2024-04-07 22:50:10 +0300

    New project was created. "Home work 11"

--- app.py
+++ app.py
@@ -3,16 +3,38 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
+# @app.route("/")
+# def hello():
+#     user_data = {
+#         "name":"Jill",
+#         "number": "+7 123 456 78 90",
+#         "age":"28",
+#         "email":"jill@gmail.com",
+#         "telegram":"jill_tellMe"
+#     }
+#     return render_template("hello.html",
+#                            user = user_data)
+
+
+# @app.route("/")
+# def hello():
+#     items = ["Python", "Java", "Kotlin", "Go"]
+#     return render_template("hello.html",
+#                            items=items)
+
+
 @app.route("/")
-def page_index():
+def hello2():
+    is_blocked = False
+    return render_template("hello.html",
+                           is_blocked=is_blocked)
 
-    page_content = """
-    <h1>Look, it is my page</h1>
-    <p>Pages is so much, but it is my page </p>
-    <p>Frontend is my very like friend</p>
-    """
 
-    return page_content
+@app.route("/1/")
+def array_start():
+    items = ["Python", "Java", "Kotlin", "Go"]
+    return render_template("array_test.html",
+                           items=items)
 
 
 app.run()
